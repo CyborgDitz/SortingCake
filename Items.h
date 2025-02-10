b23069230c095126e2c252089d7df7f6f2cea642
CyborgDitz <sebban.lol@gmail.com>
2025-02-10 14:07:38 +0100

    Added ENUM for items

--- Items.h
+++ Items.h
@@ -4,12 +4,26 @@
 
 #ifndef ITEMS_H
 #define ITEMS_H
+typedef enum {
+  ITEM_NONE,
+  ITEM_SWORD,
+  ITEM_ROD,
+  ITEM_BOW,
+  ITEM_SHIELD,
+  ITEM_ARMOR,
+  ITEM_HAT,
+  ITEM_GOLDBAR,
+  ITEM_POCKET_SAND,
+  ITEM_POCKET_GOBLIN,
+  ITEM_KEY,
+} ItemType;
 typedef struct {
     char name[10];
     int price;
     int weight;
     int quantity;
     int addedDate[10];
+  ItemType itemType;
   } Item;
 typedef struct {
   int slots[10];
